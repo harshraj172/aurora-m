32bda4177a648beea42a2f8aec89942995ea3fcc
harshraj172 <harsh777111raj@gmail.com>
2024-03-04 22:31:48 +0530

    readme update

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 #  Aurora-M
-
-This is code to finetune and run Aurora-M, an open source Starcoderplus based model trained on 400B additional tokens of multilingual and multidomain data, and adapted for multimodal understanding using the BakLLaVA/LLaVA 1.5 code base. The 400B additional tokens were trained with [BigCode's Megatron fork](https://github.com/bigcode-project/Megatron-LM). This model is intended for mixture of experts (MoE) adapation using the [M*DEL](https://huggingface.co/Multi-Domain-Expert-Learning) MoE adapatation. See our M*DEL project page for more details. 
-
-Compute provided by the LUMI Supercomputer center and JUWELS Supercomptuer center. Thank you!
-
-Also check out our BakLLaVA project, which is a cooperation between the AI Open source organizations: [LAION](https://laion.ai), [Ontocord](https://ontocord.ai), [Skunkworks OSS AI group](https://huggingface.co/SkunkworksAI) and [AI Alignment Lab](https://github.com/Alignment-Lab-AI).
-
-![Aurora Over BakLLaVA](https://github.com/ontocord/aurora-m/blob/main/Aurora_over_bakllava.png?raw=true)
+ 
+For image-to-text finetuning of `aurora-m/aurora-m-v0.1-biden-harris-redteamed`:
+```
+sbatch scripts/img2text_tuning.sh
+```
---
+++ scripts/img2text_tuning.sh
@@ -0,0 +1,128 @@
+#!/bin/bash -x
+#SBATCH --nodes=4
+#SBATCH --gres=gpu:4
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --job-name=bakllava-aurora-lumi-dev
+#SBATCH --account=cstdl
+#SBATCH --partition=develbooster
+#SBATCH --time=2:00:00
+#SBATCH --threads-per-core=1
+#SBATCH --output /p/project/ccstdl/raj3/aurora-m/slurm-output/bakllava-runs-dev-%j.out
+
+CONDA_ENV="/p/project/ccstdl/shared/generic/miniconda3/envs/raj3_bakklava"
+MINICONDA_PATH="/p/project/ccstdl/shared/generic/miniconda3"
+
+
+MODEL_VERSION="aurora-m/aurora-m-v0.1-biden-harris-redteamed"
+EXP_NAME="bakllava-img2text-$MODEL_VERSION-pretrain"
+
+# BAKLLAVA_PATH="/p/project/laionize/marianna/bakllava_original/BakLLaVA"
+BAKLLAVA_PATH="/p/project/ccstdl/raj3/aurora-m"
+
+DATA_PATH="/p/scratch/ccstdl/marianna/bakllava/blip_laion_cc_sbu_558k.json"
+# DATA_PATH="/p/scratch/ccstdl/raj3/aurora-m/blip_laion_cc_sbu_558k_text2text.json" # for text-to-text tuning 
+TEXT_DATA_PATH="/p/scratch/ccstdl/lumi-data/en/books_2k_part_aa.jsonl"
+IMAGE_FOLDER="/p/scratch/ccstdl/marianna/bakllava/images.zip"
+VISION_TOWER="openai/clip-vit-large-patch14"
+TEXTEMB_TOWER="M-CLIP/LABSE-Vit-L-14" # for text-to-text tuning
+OUTPUT_DIR="/p/scratch/ccstdl/raj3/aurora-m/checkpoints/$EXP_NAME"
+
+source ${MINICONDA_PATH}/bin/activate ${CONDA_ENV}
+
+export NCCL_IB_TIMEOUT=50
+export UCX_RC_TIMEOUT=4s
+export NCCL_IB_RETRY_CNT=10
+export NCCL_SOCKET_IFNAME=ib0
+
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
+export NCCL_ASYNC_ERROR_HANDLING=1
+
+MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
+# Allow communication over InfiniBand cells.
+
+export MASTER_ADDR="${MASTER_ADDR}.juwels"
+
+export MASTER_PORT=6000
+export NUM_NODES=4
+export NUM_GPUS=4
+# export HOSTFILE_PATH="/p/project/laionize/marianna/bakllava_original/hostfile1"
+export NCCL_DEBUG=INFO
+
+########### DO NOT CHANGE ###########
+########### USE THIS FOR BOTH ###########
+PROMPT_VERSION=plain
+########### DO NOT CHANGE ###########
+
+export PYTHONPATH="$PYTHONPATH:${BAKLLAVA_PATH}"
+
+export CUDA_DEVICE_BLOCKING=1
+# export WANDB_MODE=offline
+
+cd ${BAKLLAVA_PATH}
+
+export LAUNCHER="python -u -m torch.distributed.run \
+    --nproc_per_node $NUM_GPUS \
+    --nnodes $NUM_NODES \
+    --master_addr $MASTER_ADDR \
+    --master_port $MASTER_PORT \
+    --rdzv_endpoint $MASTER_ADDR:$MASTER_PORT \
+    --rdzv_backend c10d \
+    --max_restarts 0 \
+    --role `hostname -s`i: \
+    --tee 3 \
+    "
+
+export CMD="llava/train/train_mem.py \
+    --train_supervised True \
+    --model_name_or_path $MODEL_VERSION \
+    --cache_dir "/p/scratch/ccstdl/raj3" \
+    --version plain \
+    --dataset_type "files" \
+    --image_folder $IMAGE_FOLDER \
+    --data_path $DATA_PATH \
+    --unsupervised_data_path $TEXT_DATA_PATH \
+    --vision_tower $VISION_TOWER \
+    --text_tower $TEXTEMB_TOWER \
+    --use_text_tower False \
+    --tune_mm_mlp_adapter True \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --bf16 True \
+    --output_dir $OUTPUT_DIR \
+    --resume_from_checkpoint False \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 8 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 100 \
+    --save_total_limit 1 \
+    --learning_rate 2e-3 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 4096 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 8 \
+    --lazy_preprocess True \
+    --report_to wandb \
+    --flashattn2 True \
+    --deepspeed ./scripts/zero3.json "
+
+# MODEL_PATH_FOR_EVAL="/p/scratch/ccstdl/raj3/aurora-m/checkpoints/bakllava-quadrant-aurora-m/Aurora-90k-hf-pretrain/supervised_checkpoint-500"
+# export CMD="llava/eval/run_llava.py \
+#     --model-path $MODEL_PATH_FOR_EVAL \
+#     --image-file "https://wallpapercave.com/wp/wp1882329.jpg" \
+#     --query "what is the picture about?" \
+#     --conv-mode plain "
+
+# export CMD="llava/upload_llava.py"
+
+srun --wait=60 \
+    --kill-on-bad-exit=1 \
+     --jobid $SLURM_JOBID bash -c "$LAUNCHER --node_rank $SLURM_PROCID --role ${SLURMD_NODENAME}i: $CMD"
